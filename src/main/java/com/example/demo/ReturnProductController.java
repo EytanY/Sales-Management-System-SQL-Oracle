29ff60b979355469aa470b0a052fe84556211b3d
EytanY <88529428+EytanY@users.noreply.github.com>
2022-07-19 10:47:00 +0300

    Update

--- src/main/java/com/example/demo/ReturnProductController.java
+++ src/main/java/com/example/demo/ReturnProductController.java
@@ -4,8 +4,7 @@ package com.example.demo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-
-import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class ReturnProductController extends Controller{
     @FXML
@@ -17,8 +16,16 @@ public class ReturnProductController extends Controller{
     @FXML
     public void onReturnProductButtonClick() {
         try{
-
-
+            int deliveryID = Integer.parseInt(deliveryIDTF.getText());
+            int itemID = Integer.parseInt(itemIDTF.getText());
+            String sql = String.format("DELETE FROM %s WHERE %s = ? AND %s = ?", ITEMS_IN_DELIVERY_TABLE,
+                    ITEMS_IN_DELIVERY_DELIVERY_ID, ITEMS_IN_DELIVERY_ITEM_ID);
+            PreparedStatement statement = connection.prepareStatement(sql);
+            System.out.println(sql);
+            statement.setInt(1, deliveryID);
+            statement.setInt(2, itemID);
+            statement.executeUpdate();
+            resultLabel.setText("SUCCESS!");
         }catch (Exception exception){
             resultLabel.setText("Error!");
         }
